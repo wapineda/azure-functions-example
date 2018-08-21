868327aafb48a7bbe60c41a3b30681f7bcc6e96d
wapineda <wapineda@yahoo.com>
2018-08-21 20:49:54 -0300

    Authorization level changes

--- FunctionsExample.cs
+++ FunctionsExample.cs
@@ -13,7 +13,7 @@ namespace AzureFunctions
     public static class FunctionsExample
     {
         [FunctionName("FunctionsExample")]
-        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req, TraceWriter log)
+        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequest req, TraceWriter log)
         {
             log.Info("C# HTTP trigger function processed a request.");
 
